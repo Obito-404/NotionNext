2a11d42c3457de37031dde217654ada4a1f62cf5
Obito <54407992+Obito-404@users.noreply.github.com>
2025-01-09 11:04:31 +0800

    Update custom.js
    更新umami的参数

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,7 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
+var umamiScript = document.createElement('script');
+umamiScript.async = true;
+umamiScript.defer = true;
+umamiScript.src = 'https://umami.zerotrue.xyz/script.js';
+umamiScript.setAttribute('data-website-id', '04093dce-a621-4766-8ea3-4d1cec770a0a');
+document.head.appendChild(umamiScript);
