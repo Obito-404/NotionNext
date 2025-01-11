d299179937016b0ab0d6771cbddd4eb893f48fc0
Obito <54407992+Obito-404@users.noreply.github.com>
2025-01-11 17:09:51 +0800

    Update custom.js
    1

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,5 +1,6 @@
-var script = document.createElement('script');
-script.defer = true;
-script.src = 'https://umami.zerotrue.xyz/script.js';
-script.setAttribute('data-website-id', '9fea5bd1-a6f9-43e1-9ed4-2a39ca465129');
-document.head.appendChild(script);
+var umamiScript = document.createElement('script');
+umamiScript.async = true;
+umamiScript.defer = true;
+umamiScript.src = 'https://umami.zerotrue.xyz/script.js';
+umamiScript.setAttribute('data-website-id', '9fea5bd1-a6f9-43e1-9ed4-2a39ca465129');
+document.head.appendChild(umamiScript);
