4f2b9f7570ebee756b125df29cf6bc9232fde972
Obito <54407992+Obito-404@users.noreply.github.com>
2025-01-11 17:03:16 +0800

    Update custom.js
    更新代码

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,7 +1,5 @@
-// 这里编写自定义js脚本；将被静态引入到页面中
-var umamiScript = document.createElement('script');
-umamiScript.async = true;
-umamiScript.defer = true;
-umamiScript.src = 'https://umami.zerotrue.xyz/script.js';
-umamiScript.setAttribute('data-website-id', '9fea5bd1-a6f9-43e1-9ed4-2a39ca465129');
-document.head.appendChild(umamiScript);
+var script = document.createElement('script');
+script.defer = true;
+script.src = 'https://umami.zerotrue.xyz/script.js';
+script.setAttribute('data-website-id', '9fea5bd1-a6f9-43e1-9ed4-2a39ca465129');
+document.head.appendChild(script);
